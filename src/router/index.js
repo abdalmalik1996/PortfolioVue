82fb0bb40fb8a2c44bff29012f42e0cf6a861932
abdalmalik1996 <abd.almalik96@gmail.com>
2023-10-23 16:28:54 +0300

    Base

--- src/router/index.js
+++ src/router/index.js
@@ -1,42 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-
-
 const Home = () => import('../views/HomeView.vue')
 const About = () => import('../views/AboutView.vue')
 const Skills = () => import('../views/SkillsView.vue')
 const Work = () => import('../views/WorkView.vue')
 const Contact = () => import('../views/ContactView.vue')
 
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: About
+  },
+  {
+    path: '/skills',
+    name: 'skills',
+    component: Skills
+  },
+  {
+    path: '/work',
+    name: 'work',
+    component: Work
+  },
+  {
+    path: '/contact',
+    name: 'contact',
+    component: Contact
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_U),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: About
-    },
-    {
-      path: '/skills',
-      name: 'skills',
-      component: Skills
-    },
-    {
-      path: '/work',
-      name: 'work',
-      component: Work
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: Contact
-    }
-  ]
+  history: createWebHistory(import.meta.env.BASE_URL),
+
+  routes
 })
 
 export default router
